6d35e9e8fc46ad67c0aa033c28af6c568ccc6ee4
jesus <jesus.ramon2192@gmail.com>
2023-07-20 10:50:25 -0600

    Closures

---
+++ src/scope/re.js
@@ -0,0 +1,27 @@
+var firstName;
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = "David";
+lastName="Ana";
+console.log(lastName);
+
+var secondName = 'David';
+var secondName = 'Ana';
+console.log(secondName);
+
+let fruit = 'Apple';
+fruit= 'Kiwi';
+
+let fruit = 'Banana'
+
+const animal = 'Dog';
+//animal= 'Cat';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("Carro");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
